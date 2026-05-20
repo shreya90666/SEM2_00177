6a6063b30275da1ebb974a5c1e19c31b5949ca7c
shreya sawan <shreyasawan90@gmail.com>
2026-05-20 11:37:03 -0900

    experiment

---
+++ function.js
@@ -0,0 +1,10 @@
+function add(a,b)
+{
+    return (a+b)
+}
+console.log(addFun(7,5));
+
+(()=>console.log("hey"))();
+(function greet (){
+    console.log("hello"); 
+}
---
+++ th1.js
@@ -0,0 +1,94 @@
+
+// let name="shreya";
+// console.log(typeof name);
+// let age = 19;
+// console.log(typeof age);;
+// let isStudent= true;
+// console.log(typeof isStudent);
+// let score;
+// console.log(typeof score);
+// let car=null;
+// console.log(typeof car);
+
+// let student={
+//     name:"shreya",
+//     age:25,
+//     isStudent:true,
+//     email:"shreya90@gmail.com"
+// }
+
+// arithmetic operators 
+// let a=10;
+// let b=5;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a/b);
+// console.log(a*b);
+// console.log(a%b);
+// console.log(++a);
+// console.log(a++);
+// console.log(--a);
+// console.log(a--);
+// console.log(a);
+// assignment operator 
+// let a=10;
+// console.log(a+=3);
+// console.log(a-=3);
+// console.log(a*=3);
+// console.log(a/=3);
+// console.log(a);
+
+
+//comparsion operator 
+// let a=45;
+// let b="45";
+// console.log(a==b);
+// console.log(a===b);
+
+//string operator 
+// let a="shreya";
+// let b= "sawan";
+// console.log(a,b);
+
+// conditional operator 
+// let temp=31;
+// if(temp>30){
+//     console.log("It's a hot day");
+// }
+// else if(temp<15){
+//     console.log("It's a cold day");
+// }
+// else{
+//     console.log("It's a pleasant day");
+// }
+
+
+//switch case 
+let day = 3;
+switch (day)
+{
+     case 1:
+        console.log("monday");
+        break;
+     case 2:
+        console.log("tuesday");
+        break; 
+    case 3:
+        console.log("wednesday");
+        break;
+    case 4:
+        console.log("thursday");
+        break;
+    case 5:
+        console.log("friday");
+        break;
+    case 6:
+        console.log("saturday");
+        break;
+    case 7:
+        console.log("sunday");
+        break;
+    default :
+       console.log("invalid");
+        
+}
